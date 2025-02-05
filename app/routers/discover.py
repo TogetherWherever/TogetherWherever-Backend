351de510146368f803af235a6e5d312a0e4b32f9
Napasakorn Boonkerd <89629829+Savetang19@users.noreply.github.com>
2025-02-06 05:41:49 +0700

    feat: try to get data from api

--- app/routers/discover.py
+++ app/routers/discover.py
@@ -1,8 +1,34 @@
-from fastapi import APIRouter
+import os
+import requests
+from fastapi import APIRouter, HTTPException, Query
+from dotenv import load_dotenv
 
-router = APIRouter(prefix="/discover", tags=["discover"])
+load_dotenv()
+
+router = APIRouter(prefix="/api/discover-place-details", tags=["discover"])
+
+GOOGLE_PLACES_API_KEY = os.getenv("GOOGLE_PLACES_API_KEY")
 
 @router.get("/")
-async def get_discover():
-    # Example logic
-    return {"message": "Discover destinations!"}
+async def discover_place_details(place_id: str = Query(..., min_length=1)):
+    """
+    Fetch place details from Google Places API.
+
+    :return:
+    """
+    url = f"https://places.googleapis.com/v1/places/{place_id}"
+
+    payload = {}
+    headers = {
+        'Content-Type': 'application/json',
+        'X-Goog-Api-Key': GOOGLE_PLACES_API_KEY,
+        'X-Goog-FieldMask': '*'
+    }
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    # check if the response is successful
+    if response.status_code != 200:
+        raise HTTPException(status_code=response.status_code, detail=response.json())
+
+    return response.json()
