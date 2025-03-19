7586cafa14dd51e89fd2c55c91276de045c55907
Napasakorn Boonkerd <89629829+Savetang19@users.noreply.github.com>
2025-03-20 04:08:15 +0700

    feat: add place_id column to vote

--- app/models/vote.py
+++ app/models/vote.py
@@ -11,9 +11,10 @@ class Vote(Base):
     __tablename__ = "votes"
 
     vote_id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    username = Column(String, ForeignKey("users.username"), nullable=False)
     trip_day_id = Column(Integer, ForeignKey("trip_days.trip_day_id"), nullable=False)
+    username = Column(String, ForeignKey("users.username"), nullable=False)
     vote_status = Column(Enum("pending", "voting", "complete", name="vote_status_enum"), nullable=False)
+    place_id = Column(String, nullable=False)
     vote_score = Column(Integer, nullable=False, default=0)
 
     # Enforce vote_score range
