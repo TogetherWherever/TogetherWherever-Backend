9ad63174d5020dc687e898c9450d683f6aabf8f5
Napasakorn Boonkerd <89629829+Savetang19@users.noreply.github.com>
2025-03-22 04:38:43 +0700

    feat: add new function to create records in database

--- app/routers/create_new_trip.py
+++ app/routers/create_new_trip.py
@@ -5,16 +5,15 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from app.database import get_db
-from app.models import Trips, TripDays
+from app.models import Trips, TripDays, RecommendedPlaces
+from app.routers.recommendation_model import get_travel_group_preferences, get_recommendations, \
+    get_nearby_destinations_from_api
 from app.schemas import CreateNewTrip
-
-from app.routers.recommendation_model import get_travel_group_preferences, one_hot_encode_preferences, \
-    extract_group_profile, get_suitable_destinations, get_recommendations, get_nearby_destinations_from_api
 
 router = APIRouter(prefix="/api/create-new-trip", tags=["create-new-trip"])
 
 
-def create_recommendations(trip_id: int, trip: CreateNewTrip,  db: Session) -> pd.DataFrame:
+def create_recommendations(trip_id: int, trip: CreateNewTrip, db: Session) -> pd.DataFrame:
     """
     Create recommendations for a trip.
 
@@ -34,6 +33,68 @@ def create_recommendations(trip_id: int, trip: CreateNewTrip,  db: Session) -> p
     return recommendations
 
 
+def creat_new_trip_record(trip: CreateNewTrip, db: Session):
+    """
+    Create a new trip record in the database.
+
+    :param trip: The trip details.
+    :param db: Database session.
+    """
+    # Create a new trip record
+    new_trip = Trips(
+        owner=trip.owner,
+        trip_name=trip.trip_name,
+        dest_id=trip.dest_id,
+        dest_name=trip.dest_name,
+        dest_lat=trip.dest_lat,
+        dest_lon=trip.dest_lon,
+        start_date=trip.start_date,
+        end_date=trip.end_date,
+        duration=trip.duration,
+        companion=trip.companion  # Comma-separated string of companion IDs
+    )
+
+    db.add(new_trip)
+    db.commit()
+    db.refresh(new_trip)
+
+    # Create trip days
+    for day in range(trip.duration):
+        new_trip_day = TripDays(
+            trip_id=new_trip.trip_id,
+            day_number=day + 1,
+            date=trip.start_date + timedelta(days=day + 1)
+        )
+
+        db.add(new_trip_day)
+        db.commit()
+        db.refresh(new_trip_day)
+
+    return new_trip
+
+
+def create_recommendations_record(trip_id: int, recommendations: pd.DataFrame, db: Session):
+    """
+    Create a new recommendations record in the database.
+
+    :param trip_id: The ID of the trip.
+    :param recommendations: The recommendations.
+    :param db: Database session.
+    """
+    trip_day_id = db.query(TripDays).filter(TripDays.trip_id == trip_id, TripDays.day_number == 1).first().trip_day_id
+    for idx, row in recommendations.iterrows():
+        new_recommendation = RecommendedPlaces(
+            trip_id=trip_id,
+            trip_day_id=trip_day_id,
+            dest_id=row["AttractionId"],
+            dest_name=row["Attraction"],
+        )
+
+        db.add(new_recommendation)
+        db.commit()
+        db.refresh(new_recommendation)
+
+
 @router.post('/')
 async def create_new_trip(trip: CreateNewTrip, db: Session = Depends(get_db)):
     """
@@ -45,39 +106,20 @@ async def create_new_trip(trip: CreateNewTrip, db: Session = Depends(get_db)):
 
     try:
         # Create a new trip record
-        new_trip = Trips(
-            owner=trip.owner,
-            trip_name=trip.trip_name,
-            dest_id=trip.dest_id,
-            dest_name=trip.dest_name,
-            start_date=trip.start_date,
-            end_date=trip.end_date,
-            duration=trip.duration,
-            companion=trip.companion  # Comma-separated string of companion IDs
-        )
-
-        db.add(new_trip)
-        db.commit()
-        db.refresh(new_trip)
-
-        # Create trip days
-        for day in range(trip.duration):
-            new_trip_day = TripDays(
-                trip_id=new_trip.trip_id,
-                day_number=day + 1,
-                date=trip.start_date + timedelta(days=day + 1)
-            )
-
-            db.add(new_trip_day)
-            db.commit()
-            db.refresh(new_trip_day)
+        new_trip = creat_new_trip_record(trip, db)
 
         # Create recommendations
         recommendations = create_recommendations(new_trip.trip_id, trip, db)
 
-        print(recommendations.to_dict())
+        print(recommendations)
 
-        return {"message": "Trip created successfully", "trip_id": new_trip.trip_id}
+        # Create recommendations record
+        create_recommendations_record(new_trip.trip_id, recommendations, db)
+
+        return {
+            "message": "Trip created successfully",
+            "trip_id": new_trip.trip_id
+        }
 
     except Exception as e:
         db.rollback()
