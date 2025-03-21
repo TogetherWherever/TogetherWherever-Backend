79d28639acaae8d5275393929c24c3633b343c60
Napasakorn Boonkerd <89629829+Savetang19@users.noreply.github.com>
2025-03-21 18:58:46 +0700

    feat: add recommendation step

--- app/routers/create_new_trip.py
+++ app/routers/create_new_trip.py
@@ -1,5 +1,6 @@
 from datetime import timedelta
 
+import pandas as pd
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
@@ -7,7 +8,30 @@ from app.database import get_db
 from app.models import Trips, TripDays
 from app.schemas import CreateNewTrip
 
+from app.routers.recommendation_model import get_travel_group_preferences, one_hot_encode_preferences, \
+    extract_group_profile, get_suitable_destinations, get_recommendations, get_nearby_destinations_from_api
+
 router = APIRouter(prefix="/api/create-new-trip", tags=["create-new-trip"])
+
+
+def create_recommendations(trip_id: int, trip: CreateNewTrip,  db: Session) -> pd.DataFrame:
+    """
+    Create recommendations for a trip.
+
+    :param trip_id: The ID of the trip.
+    :param trip: The trip details.
+    :param db: Database session.
+    """
+    # Get the travel group preferences
+    travel_group_preferences = get_travel_group_preferences(trip_id, db)
+
+    # Get nearby destinations from Google Places API
+    nearby_places = get_nearby_destinations_from_api(trip.dest_lat, trip.dest_lon)
+
+    # Get recommendations
+    recommendations = get_recommendations(travel_group_preferences, nearby_places)
+
+    return recommendations
 
 
 @router.post('/')
@@ -47,6 +71,11 @@ async def create_new_trip(trip: CreateNewTrip, db: Session = Depends(get_db)):
             db.add(new_trip_day)
             db.commit()
             db.refresh(new_trip_day)
+
+        # Create recommendations
+        recommendations = create_recommendations(new_trip.trip_id, trip, db)
+
+        print(recommendations.to_dict())
 
         return {"message": "Trip created successfully", "trip_id": new_trip.trip_id}
 
