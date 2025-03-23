6e9db41d77d5c7c424fa9cf7cd6bfeb90a234e6b
Napasakorn Boonkerd <89629829+Savetang19@users.noreply.github.com>
2025-03-23 23:31:52 +0700

    feat: add is_voted to vote_scores table

--- app/models/vote_scores.py
+++ app/models/vote_scores.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, CheckConstraint
+from sqlalchemy import Column, Integer, String, ForeignKey, CheckConstraint, Boolean
 
 from app.models import Base
 
@@ -13,6 +13,7 @@ class VoteScores(Base):
     recommended_place_id = Column(Integer, ForeignKey("recommended_places.recommended_place_id"), nullable=False)
     username = Column(String, ForeignKey("users.username"), nullable=False)
     vote_score = Column(Integer, nullable=False, default=0)
+    is_voted = Column(Boolean, default=False)
 
     # Enforce vote_score range
     __table_args__ = (CheckConstraint("vote_score >= 0 AND vote_score <= 10", name="check_vote_score"),)
