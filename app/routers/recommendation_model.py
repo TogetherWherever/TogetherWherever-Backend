e7ef4cc1ebb589bbd70d75866673678735989fec
Napasakorn Boonkerd <89629829+Savetang19@users.noreply.github.com>
2025-03-27 00:52:44 +0700

    feat: add logic to remove previous destinations

--- app/routers/recommendation_model.py
+++ app/routers/recommendation_model.py
@@ -194,12 +194,13 @@ def rank_recommended_attractions(suitable_destinations: pd.DataFrame, group_prof
     return ranked_attractions
 
 
-def get_recommendations(travel_group: pd.DataFrame, destinations: pd.DataFrame) -> pd.DataFrame:
+def get_recommendations(travel_group: pd.DataFrame, destinations: pd.DataFrame, previous_dest: pd.DataFrame = None) -> pd.DataFrame:
     """
     Get recommendations for the travel group.
 
     :param travel_group: The dataframe containing the preferences of the travel group.
     :param destinations: The dataframe containing the destinations details.
+    :param previous_dest: The dataframe containing the previous destinations.
     :return: The ranked recommended attractions.
     """
     final_encoded_preferences = one_hot_encode_preferences(travel_group)
@@ -209,6 +210,10 @@ def get_recommendations(travel_group: pd.DataFrame, destinations: pd.DataFrame) 
     )
 
     suitable_destinations = get_suitable_destinations(destinations, group_profile_lst)
+
+    if previous_dest is not None and not previous_dest.empty:
+        suitable_destinations = suitable_destinations[~suitable_destinations['AttractionId'].isin(previous_dest['AttractionId'])]
+
     ranked_attractions = rank_recommended_attractions(suitable_destinations, group_profile_lst)
 
     return ranked_attractions.head(6)
